b8490b4a607d27ae6c6139adda495e5eae830d19
Baconstar100 <lgrilli03@gmail.com>
2024-07-10 15:55:26 -0400

    first

---
+++ server.cpp
@@ -0,0 +1,29 @@
+//#define CPPHTTPLIB_OPENSSL_SUPPORT
+#include <fstream>
+#include <string>
+#include <httplib.h>
+
+std::string ReadHtmlFile(std::string path) {
+	std::string zz, final;
+	std::ifstream fileRead(path);
+
+	while (std::getline(fileRead, zz)) {
+		final += zz;
+	}
+
+	fileRead.close();
+
+	return final;
+}
+
+int main() {
+	httplib::Server scr;
+
+	scr.Get("/", [](const httplib::Request &, httplib::Response &res) {	
+		res.set_content(ReadHtmlFile("./Calendar.html"), "text/html");
+	});
+
+	scr.listen("0.0.0.0", 8080);
+
+	return 0;
+}
